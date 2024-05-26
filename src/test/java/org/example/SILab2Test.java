83c42483db7ab832b269b2e717b3968ba544d725
Nikola Stojchevski <nikolaorizari@hotmail.com>
2024-05-26 23:59:50 +0200

    Commit
    Commit

---
+++ src/test/java/org/example/SILab2Test.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.example.SILab2.checkCart;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void checkCartTrue() {
+       // assertTrue(checkCart());
+    }
+    @Test
+    void checkCartFalse() {
+       // assertFalse(checkCart());
+    }
+
+}
